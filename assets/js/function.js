f470013ae828d0d16a88b1f723ae4cd5323f3d54
Robi Ratno <robi.r.mazumder@gmail.com>
2020-04-22 01:55:18 +0600

    design updated

--- assets/js/function.js
+++ assets/js/function.js
@@ -2,15 +2,19 @@ var app = angular.module('myApp', []);
 app.controller("appController", function($scope, $http) {
     $http.get("https://fastaar.com/api").then(
         function successCallback(response) {
-            $scope.confirmedToday = response.data.today.todayConfirmed;
-            $scope.recoveredToday = response.data.today.todayRecovered;
-            $scope.deathsToday = response.data.today.todayDeaths;
+            $scope.confirmedToday = response.data.new.confirmed;
+            $scope.recoveredToday = response.data.new.recovered;
+            $scope.deathsToday = response.data.new.deaths;
+            $scope.testedToday = response.data.new.tested;
             $scope.confirmedTotal = response.data.total.confirmed;
             $scope.recoveredTotal = response.data.total.recovered;
             $scope.deathsTotal = response.data.total.deaths;
+            $scope.testedTotal = response.data.total.tested;
             $scope.getDistrictsData = response.data.districts;
             $scope.datetime = response.data.lastUpdate;
-            $scope.lastUpdateData = $scope.datetime.replace(" ", 'T') + 'Z';
+            //$scope.lastUpdateData = $scope.datetime.replace(" ", 'T') + 'Z';
+            $scope.lastUpdateData = $scope.datetime;
+
             angular.element(document).ready( function () {
                 dTable = $('#districtData');
                 dTable.DataTable({
