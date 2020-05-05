97d8f3c32b0985457a0a5ea8d90857cbbdfd60a7
Robi Ratno <robi.r.mazumder@gmail.com>
2020-05-05 10:53:06 +0600

    updated aith api

--- assets/js/function.js
+++ assets/js/function.js
@@ -1,20 +1,36 @@
 var app = angular.module('myApp', []);
 app.controller("appController", function($scope, $http, $filter) {
-    $http.get("https://fastaar.com/api").then(
+    $http.get("https://corona.in.com.bd/api/stats").then(
         function successCallback(response) {
-            $scope.confirmedToday = response.data.new.confirmed;
-            $scope.recoveredToday = response.data.new.recovered;
-            $scope.deathsToday = response.data.new.deaths;
-            $scope.testedToday = response.data.new.tested;
+
+            $scope.confirmedToday = response.data.last.confirmed;
+            $scope.recoveredToday = response.data.last.recovered;
+            $scope.deathsToday = response.data.last.deaths;
+            $scope.testedToday = response.data.last.tested;
             $scope.confirmedTotal = response.data.total.confirmed;
             $scope.recoveredTotal = response.data.total.recovered;
             $scope.deathsTotal = response.data.total.deaths;
             $scope.testedTotal = response.data.total.tested;
-            $scope.getDistrictsData = response.data.districts;
+
+            $http.get("https://corona.in.com.bd/api/districts").then( function successCallback(res) {
+                $scope.getDistrictsData = res.data.data;
+
+                angular.element(document).ready( function () {
+                    var dTable = $('#districtData');
+                    dTable.DataTable({
+                        responsive: true,
+                        "lengthMenu": [[15, 30, -1], [15, 30, "All"]],
+                    });
+                    // $('input[type = search]').on( 'keyup', function () {
+                    //     console.log("1");
+                    //     dTable.columns( 1 ).search( '^' + this.value, true, false).draw();
+                    // } );
+                });
+            });
+
             $scope.datetime = response.data.lastUpdate;
             //$scope.lastUpdateData = $scope.datetime.replace(" ", 'T') + 'Z';
             $scope.lastUpdateData = $scope.datetime;
-
             $scope.lastData = parseInt($filter('date')($scope.datetime, 'dd'));
             $scope.CurrentDateTime = parseInt($filter('date')(new Date(), 'dd'));
             if($scope.lastData === $scope.CurrentDateTime){
@@ -25,13 +41,6 @@ app.controller("appController", function($scope, $http, $filter) {
                 $scope.text = 'yesterday';
                 $scope.Text = 'Yesterday';
             }
-            angular.element(document).ready( function () {
-                dTable = $('#districtData');
-                dTable.DataTable({
-                    responsive: true,
-                    "lengthMenu": [[15, 30, -1], [15, 30, "All"]]
-                });
-            });
         },
         function errorCallback(response){
             console.log("Unable to perform get request");
